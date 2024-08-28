dd223d924164648f889ee44395c6097c7a354a96
Gábor Bernát Szabó <noideaemailcim@gmail.com>
2024-08-28 14:51:22 +0200

    video based

---
+++ app/(auth)/layout.js
@@ -0,0 +1,21 @@
+import { loguot } from '@/actions/auth-actions';
+import '../globals.css';
+ 
+export const metadata = {
+  title: 'Next Auth',
+  description: 'Next.js Authentication',
+};
+ 
+export default function AuthRootLayout({ children }) {
+  return (
+    <>
+      <header id="auth-header">
+        <p>Welcome back!</p>
+        <form action={loguot}>
+          <button>Logout</button>
+        </form>
+      </header>
+      {children}
+    </>
+  );
+}
